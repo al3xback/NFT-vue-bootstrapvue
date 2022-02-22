add59d9abee2057d2d8fe2d26866ddfe5f2e0e34
alex <al3xback@gmail.com>
2022-02-22 16:13:02 +0700

    change title

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+set -e
+npm run build
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+git branch -m main
+git push -f git@github.com:al3xback/NFT-vue-bootstrapvue.git main:gh-pages
+
+cd -
